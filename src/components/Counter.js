ef1a9fe556a182999beb56bea72a92feba1c7743
Smith Scarborough <smithscarborough@gmail.com>
2021-05-19 15:28:07 -0500

    remove separate function for onClick and instead added them in-line

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,18 +3,10 @@ import React, { useState } from 'react';
 export default function Counter() {
     const [count, setCount] = useState(0);
 
-    let handleDecrement = () => {
-        setCount(count - 1);
-    }
-
-    let handleIncrement = () => {
-        setCount(count + 1);
-    }
-
     return (
         <div>
             <p>Current count: {count}</p>
-            <button onClick={handleDecrement}>Decrement</button> <button onClick={handleIncrement}>Increment</button> 
+            <button onClick={() => setCount(count - 1)}>Decrement</button> <button onClick={() => setCount(count + 1)}>Increment</button> 
         </div>
     );
 };
